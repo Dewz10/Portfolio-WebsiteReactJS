6a1be7f61e53653c4b201de5fd475ab20cb7e261
autsadawut <autsadawut.dew10@gmail.com>
2022-12-31 16:15:43 +0700

    update items-icon-socials

--- Portfolio/src/components/Socials.js
+++ Portfolio/src/components/Socials.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Socials = () => {
-    return (
-        <div>Socials</div>
-    )
-}
+import { social } from "../data";
 
-export default Socials
+const Socials = () => {
+  return (
+    <ul className="flex space-x-6">
+      {social.map((item, index) => {
+        return (
+          <li className="flex justify-center items-center text-accent">
+            <a className="text-base" href={item.href}>
+                {item.icon}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Socials;
